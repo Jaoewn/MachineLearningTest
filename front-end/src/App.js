aeb8615c269aecae7990c30142ec683acad2c3fe
Jaoewn <johnnguyenus@outlook.com>
2022-06-21 02:53:12 -0500

    added synaptic w/ xor logic

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,49 +1,52 @@
 import { useEffect, useState } from 'react';
 import { createArray } from './components/train';
+import { displayResults, initNetwork } from './components/synaptic';
 
 function App() {
-  const [selectedFile, setSelectedFile] = useState();
-	const [isFilePicked, setIsFilePicked] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(); // keeps file
+	const [isFilePicked, setIsFilePicked] = useState(false); // keeps track of file
+  const [textBox, setTextBox] = useState(""); // keeps track of text box
+  const [neuralNet, setNeuralNet] = useState(initNetwork()); // initalizes neural net
 
-  const changeHandler = (e) => {
+  // FILE HANDLING
+  const fileChangeHandler = (e) => {
 		setSelectedFile(e.target.files[0]);
 		setIsFilePicked(true);
 	};
-
-	const handleSubmission = () => {
+	const handleFileSubmission = () => {
     console.log("submit!")
     if (isFilePicked) {createArray(selectedFile)};
 	};
 
+  // TEXTBOX HANDLING
+  const textChangeHandler = (e) => {
+		setTextBox(e.target.value);
+	};
+  const handleTextSubmission = () => {
+    console.log("submit!")
+    displayResults(textBox, neuralNet);
+	};
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>WELCOME TO MACHNE LEARNING TEST</h1>
+        <p>it is currently set up for XOR on textbox[0, 1]</p>
       </header>
 
       {/* MY CODE */}
       <div>
-        <h1>INPUT CSV HERE: </h1>
+        <h3>INPUT TRAINING CSV HERE: </h3>
         <input
           type="file"
           accept=".csv" 
-          onChange={changeHandler}
+          onChange={fileChangeHandler}
         />
-        <button hidden={isFilePicked ? "" : "hidden"} onClick={handleSubmission}>Submit</button>
+        <button hidden={isFilePicked ? "" : "hidden"} onClick={handleFileSubmission}>Submit</button>
       </div>
       <div>
-        <div><textarea id="textbox" rows="20" cols="100"></textarea></div>
+        <div><textarea id="textbox" rows="2" cols="30" onChange={textChangeHandler}></textarea></div>
+        <button onClick={handleTextSubmission}>Submit</button>
         <div id="results"></div>
       </div>
     </div>
---
+++ front-end/src/components/train.js
@@ -0,0 +1,23 @@
+const createArray = (csv) => {
+  var array = []; // empty array for returning
+  let reader = new FileReader();
+  reader.addEventListener("loadend", () => {
+    // Wait until file is loaded, then process CSV
+    let data = reader.result.slice(reader.result.indexOf('\n') + 1).split("\n");
+    for (let i in data) {
+      if (data[i] !== '') {
+        data[i] = data[i].split(",");
+        data[i] = {input: data[i][0], output: data[i][1]};
+      }
+    }
+    if (data[data.length -1] === '') {data.pop()}; // popping last item if its empty
+    // data = JSON.stringify(data);
+    console.log(data);
+    array = data;
+  });
+  // Actual execution of file read
+  reader.readAsText(csv);
+  return array;
+};
+
+export { createArray };
